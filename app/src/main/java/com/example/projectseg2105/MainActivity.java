499d261428bf42173e22a175d957ae92e7c35cea
Alois Clerc <alois.clerc@gmail.com>
2020-10-07 18:10:42 -0400

    basic navigation

--- app/src/main/java/com/example/projectseg2105/MainActivity.java
+++ app/src/main/java/com/example/projectseg2105/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.projectseg2105;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.projectseg2105.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button register;
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register = (Button) findViewById(R.id.register);
+        login = (Button) findViewById(R.id.login);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openRegister();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openLogin();
+            }
+        });
+
+
+
+    }
+
+    public void openRegister(){
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void openLogin(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
